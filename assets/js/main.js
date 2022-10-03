567ce8d0399985156cf4cd361e3c8e9f0d30bac4
LorKormi <96922045+LorKormi@users.noreply.github.com>
2022-10-03 15:21:56 +0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+jQuery(document).ready(function($) {
+
+    /* ======= Scrollspy ======= */
+    $('body').scrollspy({ target: '#header', offset: 400});
+    
+    /* ======= Fixed header when scrolled ======= */
+    
+    $(window).bind('scroll', function() {
+         if ($(window).scrollTop() > 50) {
+             $('#header').addClass('navbar-fixed-top');
+         }
+         else {
+             $('#header').removeClass('navbar-fixed-top');
+         }
+    });
+   
